e6ce0057492f6c3eb9adcabbff379634ac7cf382
cartiles0 <61708300+cartiles0@users.noreply.github.com>
2020-05-05 20:17:02 +0100

    movement achived

--- game.js
+++ game.js
@@ -1,3 +1,26 @@
-function bouncyGame () {
-  var self = this;
+function BouncyGame () {
+  this.ball = document.getElementById("ball");
+  this.ballLeft = 275;
+  this.ballTop = 725;
+  this.directionY = 1
+  this.directionX = 1
+
+  this.move = function () {   
+    var self = this;
+    setInterval(function () {
+      console.log('left', self.ballTop)
+      console.log('top', self.ballLeft)
+
+      self.ball.style.left = self.ballLeft + 'px' ;
+      self.ball.style.top = self.ballTop + 'px' ;
+      
+      self.ballLeft -= 10
+      self.ballTop -= 10
+    }, 500)
+  }
+
 }
+
+var game = new BouncyGame();
+
+game.move();
