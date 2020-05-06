440fac4049b4a2b595074ba834ca9151561061aa
cartiles0 <61708300+cartiles0@users.noreply.github.com>
2020-05-06 17:30:26 +0100

    changes made to try have the ball bounce

--- game.js
+++ game.js
@@ -1,22 +1,35 @@
 function BouncyGame () {
+  var self = this;
   this.ball = document.getElementById("ball");
   this.ballLeft = 275;
   this.ballTop = 725;
-  this.directionY = 1
-  this.directionX = 1
+  this.directionY = 1;
+  this.directionX = 1;
+
+  this.gameInit = 
 
   this.move = function () {   
-    var self = this;
+    
+    var directionX = this.directionX;
+    var directionY = this.directionY;
     setInterval(function () {
-      console.log('left', self.ballTop)
-      console.log('top', self.ballLeft)
-
-      self.ball.style.left = self.ballLeft + 'px' ;
-      self.ball.style.top = self.ballTop + 'px' ;
+      var left = self.ball.style.left;
+      var top = self.ball.style.top;
+      console.log('top', self.ballTop)
+      console.log('left', self.ballLeft)
       
-      self.ballLeft -= 10
-      self.ballTop -= 10
-    }, 500)
+      self.ball.style.left = self.ballLeft + 'px';
+      self.ball.style.top = self.ballTop + 'px';
+      self.ballLeft -= 10 * directionX; 
+      self.ballTop -= 10 * directionY;
+
+      var leftLimit = left.substring(0, left.length - 2);
+
+      if (left.substring(0, left.length - 2) <= 5) {
+        directionX *= -1;
+      }
+
+    }, 200)
   }
 
 }
