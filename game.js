e35813a91401ececa6b03969083bae8a88f248a4
cartiles0 <61708300+cartiles0@users.noreply.github.com>
2020-05-06 19:22:43 +0100

    code perfected - added extra parameters and eliminated redundancies

--- game.js
+++ game.js
@@ -6,39 +6,46 @@ function BouncyGame () {
   this.directionY = 1;
   this.directionX = 1;
 
-  this.gameInit = function() {
-    this.move();
+  this.init = function () {
+    this.movement();
   }
 
-  this.move = function () {   
-    
-    var directionX = this.directionX;
-    var directionY = this.directionY;
+  this.movement = function () {   
     setInterval(function () {
-      var left = self.ball.style.left;
-      var top = self.ball.style.top;
-      
       self.ball.style.left = self.ballLeft + 'px';
       self.ball.style.top = self.ballTop + 'px';
-      self.ballLeft -= 10 * directionX; 
-      self.ballTop -= 10 * directionY;
-
-      console.log('top', self.ballTop);
-      console.log('left', self.ballLeft);
+      self.ballLeft -= 10 * self.directionX; 
+      self.ballTop -= 10 * self.directionY;
 
       if (self.ballLeft <= 0 || self.ballLeft >= 550) {
-        directionX *= -1;
+        self.directionX *= -1;
       }
-      if (self.ballTop <= 0 || self.ballTop >= 750) {
-        directionY *= -1;
+      if (self.ballTop >= 750) {
+        self.directionY *= -1;
       }
-    
-
+      if (self.ballTop <= 0) {
+        if (self.ballLeft < 260 || self.ballLeft > 340) {
+          self.directionY *= -1;
+        }
+        if (self.ballLeft >= 260 && self.ballLeft <= 340) {
+          console.log("LevelUp");
+        }
+      }
+      /*if (self.ballTop <= 0 || self.ballTop >= 750) {
+        if (self.ballTop <= 0 && self.ballLeft >= 260 && self.ballLeft <= 340) {
+          console.log("LevelUp");
+        }
+        else {self.directionY *= -1}
+      }*/
     }, 50)
+  }
+
+  this.levelUp = function() {
+
   }
 
 }
 
 var game = new BouncyGame();
 
-game.gameInit();
+game.init();
