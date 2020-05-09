ba1d0bcbaf59cf0212304244d7ce657e8c188b6a
cartiles0 <61708300+cartiles0@users.noreply.github.com>
2020-05-09 13:40:43 +0100

    added graphics and obs 1 movement

--- level1.js
+++ level1.js
@@ -4,6 +4,33 @@ class level1 extends Phaser.Scene {
   }
 
   preload() {
+    this.load.image('board', 'assets/board.png');
+    this.load.image('obs1', 'assets/obs1.png');
+    this.load.image('obs1', 'assets/obs2.png');
+    this.load.image('ball', 'assets/ball.png');
+  }
+
+  create() {
+    this.board = this.add.image(300, 400, 'board');
+
+    this.block1 = this.add.image(130, 200, 'obs1');
+    this.block2 = this.add.image(475, 350, 'obs1');
+
+    this.ball = this.add.image(300, 750, 'ball');
     
+    this.tweenBlock2 = this.tweens.add({
+      targets: this.block1,
+      x: 475,
+      ease: 'Sine.easeInOut',
+      yoyo: true,
+      repeat: 500
+    })
+    this.tweenBlock2 = this.tweens.add({
+      targets: this.block2,
+      x: 130,
+      ease: 'Sine.easeInOut',
+      yoyo: true,
+      repeat: 500
+    })
   }
 }
