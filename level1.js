a6399e1aa9c4a755f1410f6c4bb6cd9e7381e951
cartiles0 <61708300+cartiles0@users.noreply.github.com>
2020-05-12 22:33:19 +0100

    shooting ball bounces

--- level1.js
+++ level1.js
@@ -19,11 +19,13 @@ class level1 extends Phaser.Scene {
     // Cannon ball
     var cannon = this.add.image(300, 750, 'ball');
     var ball = this.physics.add.sprite(cannon.x, cannon.y, 'ball').setScale(.5);
-    var gfx = this.add.graphics().setDefaultStyles({ lineStyle: { width: 10, color: 0xffdd00, alpha: 0.5 } });
+    var gfx = this.add.graphics().setDefaultStyles({ lineStyle: { width: 5, color: 0xdddf99, alpha: 0.5 } });
     var line = new Phaser.Geom.Line();
     var angle = 0;
 
+    ball.setCollideWorldBounds(true).setBounce(1);
     ball.disableBody(true, true);
+    ball.body.setCircle(25);
 
     this.input.on('pointermove', function (pointer) {
       angle = Phaser.Math.Angle.BetweenPoints(cannon, pointer);
@@ -44,7 +46,7 @@ class level1 extends Phaser.Scene {
 
     // Yellow Obstacle
     this.yell1 = this.physics.add.sprite(100, 550, 'obs2');
-    this.yell1.setImmovable().body.setAllowGravity(false);
+    //this.yell1.setImmovable().body.setAllowGravity(false);
     this.yell2 = this.physics.add.sprite(300, 550, 'obs2');
     this.yell2.setImmovable().body.setAllowGravity(false);
     this.yell3 = this.physics.add.sprite(500, 550, 'obs2');
@@ -57,11 +59,11 @@ class level1 extends Phaser.Scene {
     //this.ball.body.velocity.setTo(-200, -200);
 
     // Colliders
-    this.physics.add.collider(this.ball, this.green1);
-    this.physics.add.collider(this.ball, this.green2);
-    this.physics.add.collider(this.ball, this.yell1);
-    this.physics.add.collider(this.ball, this.yell2);
-    this.physics.add.collider(this.ball, this.yell3);
+    this.physics.add.collider(ball, this.green1);
+    this.physics.add.collider(ball, this.green2);
+    this.physics.add.collider(ball, this.yell1);
+    this.physics.add.collider(ball, this.yell2);
+    this.physics.add.collider(ball, this.yell3);
     
     // Keyboards
     this.key_W = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
