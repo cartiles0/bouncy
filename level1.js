2e1025cccebcecc5fc7ba2fcb29107ece54d3f94
cartiles0 <61708300+cartiles0@users.noreply.github.com>
2020-05-13 17:08:17 +0100

    simplyfied coding

--- level1.js
+++ level1.js
@@ -12,15 +12,19 @@ class level1 extends Phaser.Scene {
   }
 
   create() {
+    // Objects Directions
     this.dirG1 = 1;
     this.dirG2 = -1;
     this.dirY1 = -1;
     this.dirY2 = -1;
     this.dirY3 = 1;
+
+    // Background & Top
     this.board = this.add.sprite(300, 400, 'board').setScale(.24);
+    this.board = this.add.sprite(300, 45, 'top').setScale(.25);
 
     // Cannon & Ball
-    this.cannon = this.add.image(300, 750, 'ball').setScale(.4);
+    this.cannon = this.add.image(300, 750, 'ball').setScale(.3);
     this.ball = this.physics.add.sprite(this.cannon.x, this.cannon.y, 'ball');
     this.ball.setCollideWorldBounds(true).setScale(.2).setBounce(1);
     this.ball.disableBody(true, true);
@@ -38,84 +42,61 @@ class level1 extends Phaser.Scene {
 
     this.input.on('pointerup', function () {
       this.ball.enableBody(true, this.cannon.x, this.cannon.y, true, true);
-      this.physics.velocityFromRotation(this.angle, 600, this.ball.body.velocity);
+      this.physics.velocityFromRotation(this.angle, 1200, this.ball.body.velocity);
     }, this);
 
     // Green Obstacle
-    this.green1 = this.physics.add.sprite(155, 185, 'obs1').setScale(.3);
-    this.green1.setImmovable().body.setAllowGravity(false);
-    this.green2 = this.physics.add.sprite(445, 335, 'obs1').setScale(.3);;
-    this.green2.setImmovable().body.setAllowGravity(false);
+    this.pastry1 = this.physics.add.sprite(155, 185, 'obs1').setScale(.3);
+    this.pastry1.setImmovable().body.setAllowGravity(false);
+    this.pastry2 = this.physics.add.sprite(445, 335, 'obs1').setScale(.3);;
+    this.pastry2.setImmovable().body.setAllowGravity(false);
 
     // Yellow Obstacle
-    this.yell1 = this.physics.add.sprite(100, 650, 'obs2').setScale(.3);;
-    this.yell1.setImmovable().body.setAllowGravity(false);
-    this.yell2 = this.physics.add.sprite(300, 525, 'obs2').setScale(.3);;
-    this.yell2.setImmovable().body.setAllowGravity(false);
-    this.yell3 = this.physics.add.sprite(500, 400, 'obs2').setScale(.3);;
-    this.yell3.setImmovable().body.setAllowGravity(false);
+    this.candy1 = this.physics.add.sprite(100, 650, 'obs2').setScale(.3);;
+    this.candy1.setImmovable().body.setAllowGravity(false);
+    this.candy2 = this.physics.add.sprite(300, 525, 'obs2').setScale(.3);;
+    this.candy2.setImmovable().body.setAllowGravity(false);
+    this.candy3 = this.physics.add.sprite(500, 400, 'obs2').setScale(.3);;
+    this.candy3.setImmovable().body.setAllowGravity(false);
 
     // Colliders
-    this.physics.add.collider(this.ball, this.green1);
-    this.physics.add.collider(this.ball, this.green2);
-    this.physics.add.collider(this.ball, this.yell1);
-    this.physics.add.collider(this.ball, this.yell2);
-    this.physics.add.collider(this.ball, this.yell3);
-    
-    // Keyboards
-    this.key_W = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-    this.key_S = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-    this.key_A = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-    this.key_D = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+    this.physics.add.collider(this.ball, this.pastry1);
+    this.physics.add.collider(this.ball, this.pastry2);
+    this.physics.add.collider(this.ball, this.candy1);
+    this.physics.add.collider(this.ball, this.candy2);
+    this.physics.add.collider(this.ball, this.candy3);
   }
   
   update() {
-    this.green1.x += 2 * this.dirG1;
-    if (this.green1.x <= 155) { this.dirG1 *= -1 } 
-    if (this.green1.x >= 445) { this.dirG1 *= -1 }
+    this.speedPastry = 5;
+    this.speedCandy = 3; 
+    this.pastry1.x += this.speedPastry * this.dirG1;
+    if (this.pastry1.x <= 155) { this.dirG1 *= -1 } 
+    if (this.pastry1.x >= 445) { this.dirG1 *= -1 }
     
-    this.green2.x += 2 * this.dirG2;
-    if (this.green2.x <= 155) { this.dirG2 *= -1 }
-    if (this.green2.x >= 445) { this.dirG2 *= -1 }
+    this.pastry2.x += this.speedPastry * this.dirG2;
+    if (this.pastry2.x <= 155) { this.dirG2 *= -1 }
+    if (this.pastry2.x >= 445) { this.dirG2 *= -1 }
 
-    this.yell1.y += 2 * this.dirY1;
-    if (this.yell1.y <= 400) { this.dirY1 *= -1 }
-    if (this.yell1.y >= 650) { this.dirY1 *= -1 }
+    this.candy1.y += this.speedCandy * this.dirY1;
+    if (this.candy1.y <= 400) { this.dirY1 *= -1 }
+    if (this.candy1.y >= 650) { this.dirY1 *= -1 }
 
-    this.yell2.y += 2 * this.dirY2;
-    if (this.yell2.y <= 400) { this.dirY2 *= -1 }
-    if (this.yell2.y >= 650) { this.dirY2 *= -1 }
+    this.candy2.y += this.speedCandy * this.dirY2;
+    if (this.candy2.y <= 400) { this.dirY2 *= -1 }
+    if (this.candy2.y >= 650) { this.dirY2 *= -1 }
 
-    this.yell3.y += 2 * this.dirY3;
-    if (this.yell3.y <= 400) { this.dirY3 *= -1 }
-    if (this.yell3.y >= 650) { this.dirY3 *= -1 }
-
-
-    /*
-    this.yell1.angle += 5;
-    this.yell2.angle += 5;
-    this.yell3.angle += 5;
-    */
-    if (this.key_W.isDown) {
-      this.ball.y -= 4;
-    }
-    if (this.key_S.isDown) {
-      this.ball.y += 4;
-    }
-    if (this.key_A.isDown) {
-      this.ball.x -= 4;
-    }
-    if (this.key_D.isDown) {
-      this.ball.x += 4;
-    }
+    this.candy3.y += this.speedCandy * this.dirY3;
+    if (this.candy3.y <= 400) { this.dirY3 *= -1 }
+    if (this.candy3.y >= 650) { this.dirY3 *= -1 }
   }
 
   render() {
     this.debug.spriteInfo(this.ball, 2, 2);
-    this.debug.spriteInfo(this.green1, 32, 32);
-    this.debug.spriteInfo(this.green2, 32, 32);
-    this.debug.spriteInfo(this.yell1, 32, 32);
-    this.debug.spriteInfo(this.yell2, 32, 32);
-    this.debug.spriteInfo(this.yell3, 32, 32);
+    this.debug.spriteInfo(this.pastry1, 32, 32);
+    this.debug.spriteInfo(this.pastry2, 32, 32);
+    this.debug.spriteInfo(this.candy1, 32, 32);
+    this.debug.spriteInfo(this.candy2, 32, 32);
+    this.debug.spriteInfo(this.candy3, 32, 32);
   }
 }
