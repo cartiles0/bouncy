fad02aa81f4a0aed6bb52d2d5b4aac3be2415b07
cartiles0 <61708300+cartiles0@users.noreply.github.com>
2020-05-13 13:46:53 +0100

    code simplyfied arrow key inputs added

--- level1.js
+++ level1.js
@@ -9,18 +9,6 @@ class level1 extends Phaser.Scene {
     this.load.image('obs2', 'assets/obs2.png');
     this.load.image('ball', 'assets/ball.png');
   }
-  /*
-  fire(shooter) {
-    //this.setRotation(shooter.rotation)
-
-    // Offset the bullet to start a bit right of the shooter
-    this.x = this.angle.x;
-    this.y = this.angle.y;
-
-    this.setVelocityX(BULLET_SPEED * Math.cos(Math.PI * this.angle / 180))
-    this.setVelocityY(BULLET_SPEED * Math.sin(Math.PI * this.angle / 180))
-  }*/
-
 
   create() {
     this.matter.world.setBounds(0, 0, game.config.width, game.config.height);
@@ -28,28 +16,6 @@ class level1 extends Phaser.Scene {
     this.dir1 = 1;
     this.dir2 = -1;
     this.board = this.add.sprite(300, 400, 'board');
-    
-    // Shooting Ball
-    /*this.ball = this.matter.add.sprite(300, 750, 'ball').setScale(.5).setCircle(12.5);
-    this.cannon = this.add.image(300, 750, 'ball');
-    this.gfx = this.add.graphics().setDefaultStyles({ lineStyle: { width: 10, color: 0xffdd00, alpha: 0.5 } });
-    this.line = new Phaser.Geom.Line();
-    this.angle = 0;
-    console.log(this.ball);
-    //this.ball.disableBody(true, true);
-
-    this.input.on('pointermove', function (pointer) {
-      this.angle = Phaser.Math.Angle.BetweenPoints(this.cannon, pointer);
-      Phaser.Geom.Line.SetToAngle(this.line, this.cannon.x, this.cannon.y, this.angle, 128);
-      this.gfx.clear().strokeLineShape(this.line);
-    }, this);
-
-    this.input.on('pointerup', function () {
-      console.log("hola");
-      //this.ball.enableBody(true, this.cannon.x, this.cannon.y, true, true);
-      //this.matter.velocityFromRotation(this.angle, 600, this.ball.body.velocity);
-      this.ball.fire(this.cannon);
-    }, this);*/
 
     // Green Obstacles
     this.green1 = this.matter.add.sprite(130, 190, 'obs1', null, { isStatic: true });
@@ -60,14 +26,18 @@ class level1 extends Phaser.Scene {
     this.yell2 = this.matter.add.sprite(300, 550, 'obs2', null, { isStatic: true });
     this.yell3 = this.matter.add.sprite(500, 550, 'obs2', null, { isStatic: true });
 
-    this.ball = this.matter.add.sprite(300, 750, 'ball').setCircle(12.5).setScale(.5);
-    //his.ball.setCollideWorldBounds(true).setBounce(1);
+    this.ball = this.matter.add.sprite(300, 750, 'ball').setCircle(25).setScale(.5);
 
     //Key Controls
     this.key_W = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
     this.key_S = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
     this.key_A = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     this.key_D = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+
+    this.key_Up = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+    this.key_Down = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+    this.key_Left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+    this.key_Right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     
   }
   
@@ -84,16 +54,16 @@ class level1 extends Phaser.Scene {
     this.yell2.angle += 5;
     this.yell3.angle += 5;
     
-    if (this.key_W.isDown) {
+    if (this.key_W.isDown || this.key_Up.isDown) {
       this.ball.y -= 4;
     }
-    if (this.key_S.isDown) {
+    if (this.key_S.isDown || this.key_Down.isDown) {
       this.ball.y += 4;
     }
-    if (this.key_A.isDown) {
+    if (this.key_A.isDown || this.key_Left.isDown) {
       this.ball.x -= 4;
     }
-    if (this.key_D.isDown) {
+    if (this.key_D.isDown || this.key_Right.isDown) {
       this.ball.x += 4;
     }
   }
