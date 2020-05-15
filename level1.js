c94d887fd062287aaaa7eb08e024f271ded4e16a
cartiles0 <61708300+cartiles0@users.noreply.github.com>
2020-05-15 18:55:22 +0100

    added preload

--- level1.js
+++ level1.js
@@ -7,6 +7,32 @@ class level1 extends Phaser.Scene {
     this.score = data.score;
   }
 
+  preload() {
+    this.load.image('board', 'assets/board.png');
+    this.load.image('obs1', 'assets/obs1.png');
+    this.load.image('obs2', 'assets/obs2.png');
+    this.load.image('ball', 'assets/ball.png');
+    this.load.image('top', 'assets/top.png');
+    this.load.image('obs1Long', 'assets/obs1Long.png');
+    this.load.image('PriceCoin', 'assets/PriceCoin.png');
+    this.load.image('PriceFresa', 'assets/PriceFresa.png');
+    this.load.image('PriceMenta', 'assets/PriceMenta.png');
+    this.load.image('TopL', 'assets/TopL.png');
+    this.load.image('TopR', 'assets/TopR.png');
+    this.load.image('TopOraL', 'assets/TopOraL.png');
+    this.load.image('TopOraR', 'assets/TopOraR.png');
+    this.load.image('TopPinkL', 'assets/TopPinkL.png');
+    this.load.image('TopPinkR', 'assets/TopPinkR.png');
+    this.load.image('bottom', 'assets/bottom.png');
+    this.load.image('cannon', 'assets/cannon.png');
+    this.load.image('ballGreen', 'assets/ballGreen.png');
+    this.load.image('background', 'assets/background.jpg');
+    this.load.image('YouWon', 'assets/YouWon.png');
+    this.load.image('GumBall', 'assets/GumBall.png');
+    this.load.image('GameOver', 'assets/GameOver.png');
+    this.load.image('GreatJob', 'assets/GreatJob.png');
+  }
+
   create() {
     // Objects Directions
     this.gameOn = false;
